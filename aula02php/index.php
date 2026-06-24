c056f19ddcd94596b3d1a6d1cce1a8495f50f151
Fleck <heitor.smadeira@aluno.edu.es.gov.br>
2026-06-24 11:08:52 -0300

    Create index.php

---
+++ aula02php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset= "UTF-8">
+        <title> Aula 02</title>
+</head>
+<body> 
+
+     <?php 
+        $nome = "heitor";
+        $idade = 15;
+        echo'Nome: ' .$nome. ' ,idade: ' .$idade ;
+    ?>
+      
+</body>
+</html>
